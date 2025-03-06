6ba6237b0f8d883b715449ca22bdef955fc4d6d5
Cainã Oliveira <csoliveira01@gmail.com>
2025-03-06 19:07:08 -0400

    Initial commit

---
+++ composeApp/src/commonMain/kotlin/com/cso/Greeting.kt
@@ -0,0 +1,9 @@
+package com.cso
+
+class Greeting {
+    private val platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
---
+++ composeApp/src/commonMain/kotlin/com/cso/Platform.kt
@@ -0,0 +1,7 @@
+package com.cso
+
+interface Platform {
+    val name: String
+}
+
+expect fun getPlatform(): Platform
