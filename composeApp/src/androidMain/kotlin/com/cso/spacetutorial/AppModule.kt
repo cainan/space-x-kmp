61a6aa29d3174d55e4b276f0501063e351275e10
Cainã Oliveira <csoliveira01@gmail.com>
2025-03-08 14:49:46 -0400

    making android screen

---
+++ composeApp/src/androidMain/kotlin/com/cso/spacetutorial/AppModule.kt
@@ -0,0 +1,19 @@
+package com.cso.spacetutorial
+
+import com.cso.spacetutorial.cache.AndroidDatabaseDriverFactory
+import com.cso.spacetutorial.network.SpaceXApi
+import org.koin.android.ext.koin.androidContext
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val appModule = module {
+    single<SpaceXApi> { SpaceXApi() }
+    single<SpaceXSDK> {
+        SpaceXSDK(
+            databaseDriverFactory = AndroidDatabaseDriverFactory(
+                androidContext()
+            ), api = get()
+        )
+    }
+    viewModel { RocketLaunchViewModel(sdk = get()) }
+}
--- composeApp/src/androidMain/kotlin/com/cso/spacetutorial/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/cso/spacetutorial/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.cso
+package com.cso.spacetutorial
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.cso.spacetutorial.App
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
